0b515aa9ae47776882715b55bf4df4651ef48525
matthewkyleherren <99541432+matthewkyleherren@users.noreply.github.com>
2025-01-31 13:06:41 +0000

    jhbio

--- next.config.js
+++ next.config.js
@@ -85,10 +85,6 @@ const nextConfig = withBundleAnalyzer({
         hostname: 'img.youtube.com',
         port: '',
       },
-      {
-        protocol: 'https',
-        hostname: process.env.BLOB_STORE_ID,
-      },
     ].filter(Boolean),
   },
   sassOptions: {
